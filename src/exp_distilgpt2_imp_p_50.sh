0562e3addf112c6ba84cbad2b393b1513ec0cada
Yuanzhe Dong <yudong@nvidia.com>
2023-11-18 20:53:01 -0800

    update exp script

--- src/exp_distilgpt2_imp_p_50.sh
+++ src/exp_distilgpt2_imp_p_50.sh
@@ -9,7 +9,7 @@ pruning_number=50
 seed=42
 
 origin_model=distilgpt2
-model_name_or_path=distilgpt2
+model_name_or_path=${PWD}/models/distilgpt2_finetune
 
 
 CUDA_VISIBLE_DEVICES=$DEVICE python3 real-toxicity_test.py -origin_model $origin_model -dataset $dataset -classes $n_classes -method imp_pruning -forget_perc $forget_perc -model_name_or_path $model_name_or_path -seed $seed -b $batch_size -pruning_number $pruning_number
