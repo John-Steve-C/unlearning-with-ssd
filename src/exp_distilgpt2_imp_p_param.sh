e4f67a1bc7f818482e60b4827cc2b3ace3503391
Yuanzhe Dong <5069709+yuanzhedong@users.noreply.github.com>
2023-12-18 13:56:03 -0800

    clean code (#4)

--- src/exp_distilgpt2_imp_p_param.sh
+++ src/exp_distilgpt2_imp_p_param.sh
@@ -1,9 +1,10 @@
 DEVICE=0
 dataset=allenai/real-toxicity-prompts
 batch_size=32
-pruning_number=$1
+pruning_percent=$1
 seed=42
-model_name_or_path=${PWD}/models/distilgpt2_finetune
+model_name_or_path=${PWD}/models/distilgpt2
+forget_type="abs"
 
 
-CUDA_VISIBLE_DEVICES=$DEVICE python3 real-toxicity_test.py -dataset $dataset -method imp_pruning -model_name_or_path $model_name_or_path -seed $seed -b $batch_size -pruning_number $pruning_number
+CUDA_VISIBLE_DEVICES=$DEVICE python3 real-toxicity_test.py -dataset $dataset -method imp_pruning -model_name_or_path $model_name_or_path -seed $seed -b $batch_size -pruning_percent $pruning_percent -forget_type $forget_type
